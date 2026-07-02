27039b4091300f645ddd2faec1cf6b1dbea46c62
Atharva20055 <163835592+Atharva20055@users.noreply.github.com>
2026-07-02 15:08:30 +0530

    Add Book class with basic functionality

---
+++ Books.java
@@ -0,0 +1,41 @@
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean issued;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.issued = false;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isIssued() {
+        return issued;
+    }
+
+    public void issueBook() {
+        issued = true;
+    }
+
+    public void returnBook() {
+        issued = false;
+    }
+
+    public void displayBook() {
+        System.out.println("----------------------------");
+        System.out.println("Book ID : " + bookId);
+        System.out.println("Title   : " + title);
+        System.out.println("Author  : " + author);
+        System.out.println("Status  : " + (issued ? "Issued" : "Available"));
+    }
+}
